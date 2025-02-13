b882fc5424cc52561b9362575335e56ae53e0a2e
Tushar Tari <tushartari11@gmail.com>
2025-02-13 15:49:49 +0100

    added aggregator changes

--- inventory-service/src/main/java/com/rekreation/store/inventory/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/rekreation/store/inventory/repository/InventoryRepository.java
@@ -1,7 +1,6 @@
 package com.rekreation.store.inventory.repository;
 
 import com.rekreation.store.inventory.model.Inventory;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
