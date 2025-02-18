9978ffc5574468c468f269f75ae2bf8844bad926
Tushar Tari <tushartari11@gmail.com>
2025-02-18 16:42:00 +0100

    product service changes

--- product-service/src/main/java/com/rekreation/store/product/controller/ProductController.java
+++ product-service/src/main/java/com/rekreation/store/product/controller/ProductController.java
@@ -3,15 +3,11 @@ package com.rekreation.store.product.controller;
 import com.rekreation.store.product.dto.ProductRequest;
 import com.rekreation.store.product.dto.ProductResponse;
 import com.rekreation.store.product.service.ProductService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
--- product-service/src/main/java/com/rekreation/store/product/service/ProductService.java
+++ product-service/src/main/java/com/rekreation/store/product/service/ProductService.java
@@ -4,7 +4,9 @@ import com.rekreation.store.product.dto.ProductRequest;
 import com.rekreation.store.product.dto.ProductResponse;
 import com.rekreation.store.product.model.Product;
 import com.rekreation.store.product.repository.ProductRepository;
+
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,30 +16,30 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class ProductService {
 
-  private final ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
-  public ProductResponse createProduct(ProductRequest productRequest) {
-    Product product =
-        Product.builder()
-            .name(productRequest.name())
-            .description(productRequest.description())
-            .price(productRequest.price())
-            .build();
-    productRepository.save(product);
-    log.info("Product Created Successfully");
-    return new ProductResponse(
-        product.getId(), product.getName(), product.getDescription(), product.getPrice());
-  }
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        Product product =
+                Product.builder()
+                        .name(productRequest.name())
+                        .description(productRequest.description())
+                        .price(productRequest.price())
+                        .build();
+        productRepository.save(product);
+        log.info("Product Created Successfully");
+        return new ProductResponse(
+                product.getId(), product.getName(), product.getDescription(), product.getPrice());
+    }
 
-  public List<ProductResponse> getAllProducts() {
-    return productRepository.findAll().stream()
-        .map(
-            product ->
-                new ProductResponse(
-                    product.getId(),
-                    product.getName(),
-                    product.getDescription(),
-                    product.getPrice()))
-        .toList();
-  }
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll().stream()
+                .map(
+                        product ->
+                                new ProductResponse(
+                                        product.getId(),
+                                        product.getName(),
+                                        product.getDescription(),
+                                        product.getPrice()))
+                .toList();
+    }
 }
