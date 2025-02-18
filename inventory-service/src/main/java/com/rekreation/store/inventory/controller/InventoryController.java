4fbf50d2f91d387ab0751795a85ecca4ca4a4359
Tushar Tari <tushartari11@gmail.com>
2025-02-18 16:42:26 +0100

    inventory service changes

--- inventory-service/src/main/java/com/rekreation/store/inventory/controller/InventoryController.java
+++ inventory-service/src/main/java/com/rekreation/store/inventory/controller/InventoryController.java
@@ -3,11 +3,7 @@ package com.rekreation.store.inventory.controller;
 import com.rekreation.store.inventory.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/inventory")
