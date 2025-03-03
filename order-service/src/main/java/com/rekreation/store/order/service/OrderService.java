2828fa0c7a9b975803387c683cf7676a0a296b54
Tushar Tari <tushartari11@gmail.com>
2025-03-03 19:41:27 +0100

    fixed the error for firstname and skucode

--- order-service/src/main/java/com/rekreation/store/order/service/OrderService.java
+++ order-service/src/main/java/com/rekreation/store/order/service/OrderService.java
@@ -5,7 +5,9 @@ import com.rekreation.store.order.dto.OrderRequest;
 import com.rekreation.store.order.event.OrderPlacedEvent;
 import com.rekreation.store.order.model.Order;
 import com.rekreation.store.order.repository.OrderRepository;
+
 import java.util.UUID;
+
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,32 +19,34 @@ import org.springframework.stereotype.Service;
 public class OrderService {
 
     private static final Logger log = LoggerFactory.getLogger(OrderService.class);
-    private  final OrderRepository orderRepository;
-  private final InventoryClient inventoryClient;
-  private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
+    private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
-  public void placeOrder( OrderRequest orderRequest){
+    public void placeOrder(OrderRequest orderRequest) {
 
-   var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
-   if (isProductInStock){
-     Order order = new Order();
-     order.setOrderNumber(UUID.randomUUID().toString());
-     order.setPrice(orderRequest.price());
-     order.setSkuCode(orderRequest.skuCode());
-     order.setQuantity(orderRequest.quantity());
-     orderRepository.save(order);
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
 
-     // send message to kafka topic for order processing
-     OrderPlacedEvent orderPlacedEvent= new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+            // send message to kafka topic for order processing
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+            orderPlacedEvent.setOrderNumber(order.getOrderNumber());
+            orderPlacedEvent.setEmail(orderRequest.userDetails().email());
+            orderPlacedEvent.setFirstName(orderRequest.userDetails().firstName());
+            orderPlacedEvent.setLastName(orderRequest.userDetails().lastName());
 
-     log.info("start Sending Order Placed Event for Order Number: {}", order.getOrderNumber());
-     kafkaTemplate.send("order_placed", orderPlacedEvent);
-       log.info("end Seding Order Placed Event for Order Number: {}", order.getOrderNumber());
-   }else{
-      throw new RuntimeException("Product with SkuCode " +orderRequest.skuCode()+ "is out of stock");
-   }
-
-
-  }
+            log.info("start Sending Order Placed Event {} to kafka topic order-placed. ", orderPlacedEvent);
+            kafkaTemplate.send("order_placed", orderPlacedEvent);
+            log.info("end Sending Order Placed Event {} to kafka topic order-placed", orderPlacedEvent);
+        } else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + "is out of stock");
+        }
+    }
 }
